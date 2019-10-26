140e70d4cb72403b87c69f2da73788f864cd5431
Jules Lefebvre <juleslefebvre.pro@outlook.fr>
2019-10-26 09:47:37 +0200

    addchaging category

--- app/app.js
+++ app/app.js
@@ -4,4 +4,30 @@ window.addEventListener('load', (event) => {
   menuOpen.addEventListener('click', (event) => {
     menu.classList.toggle('close');
   });
+
+  let menuItems = [...document.getElementsByTagName('menu-item')];
+  let content = document.querySelector('content > iframe');
+  menuItems.forEach(item => {
+    if (item.attributes.page !== undefined) {
+      item.addEventListener('click', (event) => {
+          selectItem(item)
+      });
+    }
+  });
+
+  function disableItems() {
+    menuItems.forEach(item => {
+      item.classList.remove('activ');
+    });
+  }
+
+  function selectItem(item) {
+    let page = item.attributes.page.value;
+    content.setAttribute('src', `./page/${page}/${page}.html`);
+    disableItems();
+    item.classList.add('activ');
+  }
+
+  selectItem(menuItems[0]);
+
 });
